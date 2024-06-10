d99856f380cb7655295d6a62def8719ba1fd1367
23074 <2307485800@qq.com>
2024-06-10 10:40:04 +0800

    基于docker实现的代码沙箱

---
+++ src/main/java/com/lab/codesandbox/CodeSandbox.java
@@ -0,0 +1,19 @@
+package com.lab.codesandbox;
+
+
+import com.lab.codesandbox.model.ExecuteCodeRequest;
+import com.lab.codesandbox.model.ExecuteCodeResponse;
+
+/**
+ * 代码沙箱接口定义
+ */
+public interface CodeSandbox {
+
+    /**
+     * 执行代码
+     *
+     * @param executeCodeRequest
+     * @return
+     */
+    ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest);
+}
